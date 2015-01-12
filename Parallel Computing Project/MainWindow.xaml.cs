86ff20ca32ea3accd39ef7151c4b2b6fec82533d
Legiew <sebweigel@gmail.com>
2015-01-12 15:19:37 +0100

    Add first version of sync/serial search

--- Parallel Computing Project/MainWindow.xaml.cs
+++ Parallel Computing Project/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +22,58 @@ namespace Parallel_Computing_Project
    /// </summary>
    public partial class MainWindow : Window
    {
+      private FolderBrowserDialog folderBrowseDialog;
+
       public MainWindow()
       {
          InitializeComponent();
+
+         this.folderBrowseDialog = new FolderBrowserDialog();
+      }
+
+      private void buttonBrowse_Click(object sender, RoutedEventArgs e)
+      {
+         DialogResult result = this.folderBrowseDialog.ShowDialog();
+
+         if (result == System.Windows.Forms.DialogResult.OK)
+         {
+            this.textBoxDirectory.Text = this.folderBrowseDialog.SelectedPath;
+         }
+      }
+
+      private void buttonStartSearch_Click(object sender, RoutedEventArgs e)
+      {
+         this.listBoxSearchResults.ItemsSource = null;
+
+         List<string> files = this.GetFilesInDirectory(this.textBoxDirectory.Text);
+
+         List<string> foundFiles = new List<string>();
+
+         foreach (string file in files)
+         {
+            string fileContent = File.ReadAllText(file);
+
+            if (fileContent.Contains(this.textBoxSearchText.Text))
+            {
+               foundFiles.Add(file);
+            }
+         }
+
+         this.listBoxSearchResults.ItemsSource = files;
+      }
+ 
+      private List<string> GetFilesInDirectory(string directory)
+      {
+         List<string> filesCurrentDirectory = new List<string>();
+
+         filesCurrentDirectory.AddRange(Directory.GetFiles(directory));
+
+         foreach (string directoryPath in Directory.GetDirectories(directory))
+         {
+            filesCurrentDirectory.AddRange(GetFilesInDirectory(directoryPath));
+         }
+
+         return filesCurrentDirectory;
       }
    }
 }
